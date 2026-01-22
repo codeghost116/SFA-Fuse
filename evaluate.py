1fa8ba4ed474bb76a86ae53f0e1d1051995bae8c
codeghost116 <codeghost11@proton.me>
2026-01-21 16:47:55 -0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,1 +1,16 @@
+import torch
+from utils import compute_auc
+
+def evaluate(model, loader, device):
+    model.eval()
+    preds, labels = [], []
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device)
+            out = model(x)
+            preds.extend(out.cpu().numpy())
+            labels.extend(y.numpy())
+
+    return compute_auc(labels, preds)
 
