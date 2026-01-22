e6256f6a2f5b2b9382229de7041906d933462b10
codeghost116 <codeghost11@proton.me>
2026-01-21 16:46:07 -0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,1 +1,37 @@
+import os
+import cv2
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
 
+class CelebDFDataset(Dataset):
+    def __init__(self, image_paths, labels, transform=None):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        label = self.labels[idx]
+        return img, label
+
+
+class FFPPDataset(Dataset):
+    def __init__(self, frame_paths, labels, transform=None):
+        self.frame_paths = frame_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        img = Image.open(self.frame_paths[idx]).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        return img, self.labels[idx]
+
+    def __len__(self):
+        return len(self.frame_paths)
